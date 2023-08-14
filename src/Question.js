1128ad877205694e1e75bad750d147f4d7250b03
ankit gupta <ankitgupta17878@gmail.com>
2023-08-14 15:40:57 +0530

    Quiz app

---
+++ src/Question.js
@@ -0,0 +1,39 @@
+export const quizData = [{
+    question: "Which of the following is a client site language?",
+
+    Options: ['Java', 'C', 'Python', 'JavaScript'],
+
+    correct: 4,
+    marked:-1
+},
+{
+    question: "What does HTML stand for?",
+    Options: ["Hypertext Markup Language",
+        "Cascading Style Sheet",
+        "Jason Object Notation",
+        "Helicopters Terminals Motorboats Lamborginis"]
+    ,
+    correct: "1",
+    marked:-1
+},
+{
+    question: "What year was JavaScript launched?",
+    Options: ["1996",
+        "1995",
+        "1994",
+        "none of the above"]
+    ,
+    correct: "2",
+    marked:-1
+},
+{
+    question: "What does CSS stands for?",
+    Options: ["Hypertext Markup Language",
+        "Cascading Style Sheet",
+        "Jason Object Notation",
+        "Helicopters Terminals Motorboats Lamborginis"]
+    ,
+    correct: "2",
+    marked:-1
+}
+];
